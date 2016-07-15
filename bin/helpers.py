f0e3a071a6b55df166885f00ed2419235cd34d69
bernie macias <berniem@zillow.com>
2016-07-15 16:19:57 -0700

    add helper functions for custom conf

--- bin/helpers.py
+++ bin/helpers.py
@@ -3,30 +3,54 @@ import sys
 import requests
 import json
 import os
+import platform
 try:
     from splunk.clilib import cli_common as cli
 except:
     pass
 
 def splunkd_auth_header(session_key):
+    """
+    Building dict for request headers
+    :param session_key:
+    :return:
+    """
     return {'Authorization': 'Splunk ' + session_key}
 
-def update_settings(settings, server_uri, session_key):
-    r = requests.post(
-        url=server_uri+'/servicesNS/nobody/jira-send-command/configs/inputs/jirasend?output_mode=json',
-        data={
-            'url': settings.get('url'),
-            'username': settings.get('username')
-        },
-        headers=splunkd_auth_header(session_key),
-        verify=False).json()
+def update_settings(settings, server_uri, session_key, conf, password_store):
+    """
+    Updates config file and password store
+    :param settings:
+    :param server_uri:
+    :param session_key:
+    :param conf:
+    :param password_store:
+    :return:
+    """
+    app = get_appName()
+    url = "%s%s%s%s%s%s" % (server_uri, '/servicesNS/nobody/', app,'/storage/passwords/%3A', password_store,'%3A?output_mode=json')
     requests.post(
-        url=server_uri + '/servicesNS/nobody/jira-send-command/storage/passwords/%3Ajirasend_password%3A?output_mode=json',
+        url=url,
         data={
-            'password': settings.get('customcommand_password')
+            'password': settings.pop(password_store, None)
         },
         headers=splunkd_auth_header(session_key),
         verify=False)
+    update_config(conf, settings)
+
+
+def get_settings(server_uri, session_key, conf, password_store):
+    """
+    Retrieves merged custom config file and password
+    :param server_uri:
+    :param session_key:
+    :param conf:
+    :param password_store:
+    :return:
+    """
+    results = get_config(conf, local=True)
+    results[password_store] = get_password(server_uri, session_key, password_store)
+    return results
 
 def update_config(conf, stanzaDict):
     """
@@ -42,10 +66,17 @@ def update_config(conf, stanzaDict):
     return True
 
 
-
-def get_password(server_uri, session_key, app, passwordkey):
-    password_url = server_uri + '/servicesNS/nobody/jira-send-command/storage/passwords/%3Acustomcommand_password%3A?output_mode=json'
-
+def get_password(server_uri, session_key, password_store):
+    """
+    Retrives password from store in plain text
+    :param server_uri:
+    :param session_key:
+    :param password_store:
+    :return:
+    """
+    app = get_appName()
+    password_url = "%s%s%s%s%s%s" % (server_uri, '/servicesNS/nobody/', app,
+                                     '/storage/passwords/%3A', password_store, '%3A?output_mode=json')
     try:
         # attempting to retrieve cleartext password, disabling SSL verification for practical reasons
         result = requests.get(url=password_url, headers=splunkd_auth_header(session_key), verify=False)
@@ -61,11 +92,11 @@ def get_password(server_uri, session_key, app, passwordkey):
     return password
 
 
-def get_config(conf):
+def get_config(conf, local=False):
     """
     Creates a dictionary fo dicts for uses in Sorkin .conf file.
     This function creates parity for use with writeConfFile in splunk.clilib
-
+    :pram local: local config only
     :param conf: Splunk conf file file name
     :return: dictionary of dicts
     """
@@ -74,11 +105,24 @@ def get_config(conf):
     defaultconfpath = os.path.join(appdir, "default", conf)
     stanzaDict = cli.readConfFile(defaultconfpath) if os.path.exists(defaultconfpath) else {}
     localconfpath = os.path.join(appdir, "local", conf)
-    if os.path.exists(localconfpath):
-        localconf = cli.readConfFile(localconfpath)
-        for setting, stanza in localconf.items():
-            if setting in stanzaDict:
-                stanzaDict[setting].update(stanza)
-            else:
-                stanzaDict[setting] = stanza
-    print stanzaDict
+    if not local:
+        if os.path.exists(localconfpath):
+            localconf = cli.readConfFile(localconfpath)
+            for setting, stanza in localconf.items():
+                if setting in stanzaDict:
+                    stanzaDict[setting].update(stanza)
+                else:
+                    stanzaDict[setting] = stanza
+    else:
+        stanzaDict = cli.readConfFile(localconfpath) if os.path.exists(localconfpath) else {}
+    return stanzaDict
+
+def get_appName():
+    """
+    Returns current app context
+    :return:
+    """
+    appdir = os.path.dirname(os.path.dirname(__file__))
+    splitby = '/' if not (platform.system() == 'Windows') else '\\'
+    app = appdir.split(splitby)[-1]
+    return app
