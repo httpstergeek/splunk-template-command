5a59b19195c4d2fd7cdb2808b1298a51fabcb91a
bernie macias <berniem@zillow.com>
2016-07-15 16:20:38 -0700

    update view and install_endpoint for custom conf

--- bin/install_endpoint.py
+++ bin/install_endpoint.py
@@ -3,11 +3,11 @@ __author__ = ''
 
 import splunk
 import splunk.admin as admin
-from helper import *
+from helpers import *
 
 PASSWORD_PLACEHOLDER = '*******'
 
-class JiraAlertsInstallHandler(admin.MConfigHandler):
+class InstallHandler(admin.MConfigHandler):
     def __init__(self, *args):
         admin.MConfigHandler.__init__(self, *args)
         self.shouldAutoList = False
@@ -16,23 +16,26 @@ class JiraAlertsInstallHandler(admin.MConfigHandler):
         self.supportedArgs.addOptArg('*')
 
     def handleList(self, confInfo):
-        settings = get_settings(splunk.getLocalServerInfo(), self.getSessionKey())
-        item = confInfo['command']
-        item['url'] = settings.get('url', 'http://your.server/')
-        item['username'] = settings.get('username')
+        config = get_config('customcommand')
+        settings = config['customcommand'] if 'customcommand' in config else {}
+        item = confInfo['customcommand']
+        item['url'] = settings['url'] if settings['url'] else 'http://your.server/'
+        item['username'] = settings['username'] if settings['username'] else ''
         item['password'] = PASSWORD_PLACEHOLDER
 
     def handleEdit(self, confInfo):
-        if self.callerArgs.id == 'command':
-            settings = get_jira_settings(splunk.getLocalServerInfo(), self.getSessionKey())
+        if self.callerArgs.id == 'customcommand':
+            password_store = 'customcommand_password'
+            settings = get_settings(splunk.getLocalServerInfo(), self.getSessionKey(), self.callerArgs.id, password_store)
+            settings[self.callerArgs.id] = {}
             if 'url' in self.callerArgs:
-                settings['url'] = self.callerArgs['url'][0]
+                settings[self.callerArgs.id]['url'] = self.callerArgs['url'][0]
             if 'username' in self.callerArgs:
-                settings['username'] = self.callerArgs['username'][0]
+                settings[self.callerArgs.id]['username'] = self.callerArgs['username'][0]
             if 'password' in self.callerArgs:
                 password = self.callerArgs['password'][0]
                 if password and password != PASSWORD_PLACEHOLDER:
-                    settings['customcommand_password'] = password
-            update_jira_settings(jira_settings, splunk.getLocalServerInfo(), self.getSessionKey())
+                    settings[password_store] = password
+            update_settings(settings, splunk.getLocalServerInfo(), self.getSessionKey(), self.callerArgs.id, password_store)
 
-admin.init(JiraAlertsInstallHandler, admin.CONTEXT_APP_ONLY)
+admin.init(InstallHandler, admin.CONTEXT_APP_ONLY)
