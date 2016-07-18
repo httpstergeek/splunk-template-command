b25ced82dcd1042d0250fa19e0bdd1d92a1bcca7
bernie macias <berniem@zillow.com>
2016-07-18 14:11:49 -0700

    update to reflect heplers updates and bugs with

--- bin/customcommand.py
+++ bin/customcommand.py
@@ -22,24 +22,19 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option
 import sys
 import json
-from helper import get_password, get_config
+from helpers import get_password, get_config, get_appName
 
 
 @Configuration()
-class CustomCommand(StreamingCommand):
+class customCommand(StreamingCommand):
     """ Counts the number of non-overlapping matches to a regular expression in a set of fields.
     ##Syntax
     .. code-block::
-        countmatches fieldname=<field> pattern=<regular_expression> <field-list>
+        customcommand fields="<field>,*"
     ##Description
-    A count of the number of non-overlapping matches to the regular expression specified by `pattern` is computed for
-    each record processed. The result is stored in the field specified by `fieldname`. If `fieldname` exists, its value
-    is replaced. If `fieldname` does not exist, it is created. Event records are otherwise passed through to the next
-    pipeline processor unmodified.
-    ##Example
-    Count the number of words in the `text` of each tweet in tweets.csv and store the result in `word_count`.
+    Example  of building a stream custom command.
     .. code-block::
-        | inputlookup tweets | countmatches fieldname=word_count pattern="\\w+" text
+        | inputlookup tweets | customcommand fields="<field>,*"
     """
     fields = Option(
         doc='''
@@ -49,19 +44,22 @@ class CustomCommand(StreamingCommand):
 
     def stream(self, records):
         searchinfo = self.metadata.searchinfo
-        password = get_password(searchinfo.splunkd_uri, searchinfo.session_key)
-        config = get_config(searchinfo.splunkd_uri, searchinfo.session_key)
+
+        password = get_password(searchinfo.splunkd_uri, searchinfo.session_key, get_appName())
+        config = get_config('customcommand', local=True)
 
         # create outbound JSON message body
         fields = self.fields.split(',')
 
         for record in records:
-            record['fields'] = '%s' % fields
-            record['password'] = password
-            record['config'] = config
-            record['command_metadata'] = '%s' % self.metadata
+            record['a_fields'] = '%s' % fields
+            record['a_password'] = password
+            record['a_combine'] = "%s%s" % (record['sourcetype'], record['source'])
+            record['a_config'] = config
+            record['a_command_metadata'] = '%s' % self.metadata
+
             yield record
 
-dispatch(CustomCommand, sys.argv, sys.stdin, sys.stdout, __name__)
+dispatch(customCommand, sys.argv, sys.stdin, sys.stdout, __name__)
 
 
